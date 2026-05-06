da43fe28d5cdd27b0ad387747fa1c9a45487b86c
ptaki <takipemmanuel@gmail.com>
2026-05-06 17:27:29 +0100

    ongoing groupie tracker

---
+++ api/client.go
@@ -0,0 +1,101 @@
+package api
+
+import (
+  "fmt"
+  "net/http"
+  "encoding/json"
+  "strings"
+  "log"
+)
+
+func AllArtist (){
+  artists, err :=getArtists()
+  if err != nil {
+    log.Fatal("failed to get artists:", err)
+  }
+
+  relations, err := getRelations()
+  if err != nil {
+    log.Fatal("failed to get relations:", err)
+  }
+
+  relMap := make(map[int]Relation)
+
+  var artistsInfo []FullArtistInfo
+
+  for _, rel := range relations{
+    relMap[rel.Id] = rel
+  }
+  
+
+  for _, artist := range artists{
+    id := artist.Id
+    rel, ok := relMap[id]
+      if !ok {
+        log.Printf("no relation found for artist %d", id)
+        continue
+      }
+
+    info := FullArtistInfo{
+      Artist:artist,
+      DateLocations: rel.DatesLocations,
+    }
+    artistsInfo = append(artistsInfo, info)
+  }
+
+  for _, info := range artistsInfo{
+      fmt.Printf("artist:%s\n", info.Name)
+
+      for place, loc := range info.DateLocations{
+        fmt.Println(" ", place, "=>", loc)
+      }
+      fmt.Println(strings.Repeat("=",20))
+  }
+
+}
+
+
+func getArtists()([]Artist, error){
+    data,err:= http.Get("https://groupietrackers.herokuapp.com/api/artists")
+    if err != nil{
+      return nil, err
+    }
+   defer data.Body.Close()
+
+    if data.StatusCode != http.StatusOK{
+     return nil, fmt.Errorf("bad status: %s", data.Status)
+   }
+   
+   var artists []Artist
+
+
+   err = json.NewDecoder(data.Body).Decode(&artists)
+    if err != nil{
+     return nil, err
+    }
+    
+
+    return artists, nil
+}
+
+func getRelations()([]Relation, error){
+    data,err:= http.Get("https://groupietrackers.herokuapp.com/api/relation")
+    if err != nil{
+      return nil, err
+    }
+   defer data.Body.Close()
+
+   if data.StatusCode != http.StatusOK{
+     return nil, fmt.Errorf("bad status: %s", data.Status)
+   }
+   
+   var relation RelationIndex
+
+
+   err = json.NewDecoder(data.Body).Decode(&relation)
+    if err != nil{
+     return nil, err
+    }
+    
+    return relation.Index, nil
+}
