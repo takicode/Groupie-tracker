7a5f7b77fb968a6324885343d73c73ece12ad2a2
takicode <takipemmanuel@gmail.com>
2026-06-23 04:01:03 +0100

    coordinates.go done

--- internal/artist/coordinates.go
+++ internal/artist/coordinates.go
@@ -5,12 +5,42 @@ import (
     "os"
 )
 
+const coordinateFile = "data/coordinates.json"
+
 type CoordinateMap map[string]GeoLocation
 
-func LoadCoordinates(filename string) (CoordinateMap, error){
-	
+func LoadCoordinates() (CoordinateMap, error){
+	 file, err := os.ReadFile(coordinateFile)
+
+	 if err!=nil{
+		if os.IsNotExist(err){
+			return CoordinateMap{}, nil
+		}
+		return CoordinateMap{}, err
+	 }
+
+	 var coord CoordinateMap
+
+	 err:= json.UnMarshal(file, coord)
+
+	 if err!=nil{
+		return CoordinateMap{}, err
+	 }
+	 return coord, nil
 }
 
-func SaveCoordinates(filename string, coords CoordinateMap) error{
+func SaveCoordinates(coords CoordinateMap) error{
+	err := os.MkdirAll("./data", 0755)
+	if err !=nil{
+		return err
+	}
 
+    data,err:= json.MarshalIndent(coords,""," ")
+
+	err:=os.WriteFile(coordinateFile, data, 0644)
+
+	if err !=nil{
+		return err
+	}
+  return nil
 }
